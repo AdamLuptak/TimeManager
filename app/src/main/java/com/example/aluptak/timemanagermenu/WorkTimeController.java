eecce2c2376138fdb8de063dad52317014a9e499
vladislav.mino <vladislav.mino@t-systems.sk>
2016-01-27 12:48:34 +0100

    WorkTimeController - 2 new important methods for UI
    (cherry picked from commit 025bd75b00a34bea86fe5e482d20fbc42c424fec)
    
    # Conflicts:
    #	app/src/main/java/com/example/aluptak/timemanagermenu/WorkTimeController.java

--- app/src/main/java/com/example/aluptak/timemanagermenu/WorkTimeController.java
+++ app/src/main/java/com/example/aluptak/timemanagermenu/WorkTimeController.java
@@ -1,31 +1,35 @@
 package com.example.aluptak.timemanagermenu;
 
+import android.content.Context;
+import android.widget.Toast;
+
 /**
  * Created by vmino on 27/01/2016.
  */
 public class WorkTimeController {
 
-    public String getOvertime(){
-        //TODO
+    public String getOvertime() {
+// TODO: 27/01/2016
         return "+1:25";
     }
 
-    public String getLeaveTime(){
-        //TODO
+    public String getLeaveTime() {
+// TODO: 27/01/2016
         return "17:30";
     }
 
-    public String getLeaveTimeWithOT(){
-        //TODO
+    public String getLeaveTimeWithOT() {
+// TODO: 27/01/2016
         return "16:05";
     }
 
-    public void writeArrivalTime(){
-//        TODO
+    public void writeArrivalTime() {
+// TODO: 27/01/2016  \
     }
 
-    public void writeLeaveTime(){
-//        TODO
+    public void writeLeaveTime() {
+// TODO: 27/01/2016  
     }
+
 
 }
